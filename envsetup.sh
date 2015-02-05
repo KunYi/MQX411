6c57699cd671d1e2013e67e37263ed5701b8e14c
KunYi <kunyi_chen@(NB)>
2015-02-05 20:39:03 +0800

    add a environment setup script for build MQX

---
+++ envsetup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "setting build environment for MQX411 with gcc-arm-none-eabi-4_9-2014q4"
+
+# copy from AOSP
+function cgrep()
+{
+	find . -name .repo -prune -o -name .git -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.mk' -o -name 'Makefile' \) -print0 | xargs -0 grep --color -n "$@"
+}
+
+function elf2bin()
+{
+	arm-none-eabi-objcopy -O binary $@.elf $@.bin
+}
+export MQX_ROOTDIR=/home/kunyi/MQX411
+export PATH=/home/kunyi/emgcc/gcc-arm-none-eabi-4_9-2014q4/bin/:$PATH
+export GCC_REV=4.9.3
+
